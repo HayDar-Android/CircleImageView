23075f710b516174e8c31131b7460ea09aec4193
haydar <15601129849@163.com>
2016-06-12 13:22:11 +0800

    update

--- circle-imageview/src/main/java/io/haydar/civ/CircleImageView.java
+++ circle-imageview/src/main/java/io/haydar/civ/CircleImageView.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Shader;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -15,12 +17,11 @@ import android.widget.ImageView;
  */
 public class CircleImageView extends ImageView {
 
-    private Bitmap mBitmap;
-
     private Paint mPaint;
-
-    BitmapShader shader;
-    Canvas canvas1;
+    private int mRadius;
+    private Matrix mMatrix;
+    private BitmapShader mBitmapShader;
+    private int mWidth;
 
     public CircleImageView(Context context) {
         super(context);
@@ -32,6 +33,7 @@ public class CircleImageView extends ImageView {
 
     public CircleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mMatrix = new Matrix();
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setFilterBitmap(true);
@@ -39,43 +41,56 @@ public class CircleImageView extends ImageView {
         mPaint.setStrokeWidth(0.0f);
     }
 
-    private int mwidth;
-    private int mheight;
-    private int length;
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        mRadius = mWidth / 2;
+        setMeasuredDimension(mWidth, mWidth);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         if (getDrawable() == null) {
-            return; // couldn't resolve the URI
+            return;
         }
-        if (getWidth() == 0 || getHeight() == 0) {
-            return;     // nothing to draw (empty bounds)
+        setUpShader();
+        canvas.drawCircle(mRadius, mRadius, mRadius, mPaint);
+    }
+
+    private void setUpShader() {
+        Drawable drawable = getDrawable();
+        if (drawable == null) {
+            return;
+        }
+        Bitmap bmp = drawableToBitamp(drawable);
+// 将bmp作为着色器，就是在指定区域内绘制bmp
+        mBitmapShader = new BitmapShader(bmp, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        // 拿到bitmap宽或高的小值
+        float scale = 1.0f;
+        int bSize = Math.min(bmp.getWidth(), bmp.getHeight());
+        scale = mWidth * 1.0f / bSize;
+        // shader的变换矩阵，我们这里主要用于放大或者缩小
+        mMatrix.setScale(scale, scale);
+// 设置变换矩阵
+        mBitmapShader.setLocalMatrix(mMatrix);
+// 设置shader
+        mPaint.setShader(mBitmapShader);
+    }
+
+    private Bitmap drawableToBitamp(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bd = (BitmapDrawable) drawable;
+            return bd.getBitmap();
         }
 
-
-        if (getDrawable() instanceof ColorDrawable) {
-            mwidth = getDrawable().getBounds().width();
-            mheight = getDrawable().getBounds().height();
-        } else {
-            mwidth = getDrawable().getIntrinsicWidth();
-            mheight = getDrawable().getIntrinsicHeight();
-        }
-
-        int length = Math.min(mwidth, mheight);
-        mBitmap = Bitmap.createBitmap(
-                mwidth,
-                mheight, Bitmap.Config.ARGB_8888);
-        canvas1 = new Canvas(mBitmap);
-        int width = Math.min(getWidth(), getHeight());
-        getDrawable().setBounds(0, 0, mwidth, mheight);
-        float scaleWidth = width / (float) length;
-        float scaleHeight = width / (float) length;
-        canvas1.scale(scaleWidth, scaleHeight);
-        getDrawable().draw(canvas1);
-        shader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        mPaint.setShader(shader);
-        canvas.drawCircle(width / 2, width / 2, width / 2, mPaint);
-
+        int w = drawable.getBounds().width();
+        int h = drawable.getBounds().height();
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, w, h);
+        drawable.draw(canvas);
+        return bitmap;
     }
 
 }
